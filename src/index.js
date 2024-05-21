8f4ffe9504915efd13831154869fd8c12d8087db
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-21 10:26:46 -0400

    Refactor code

--- src/index.js
+++ src/index.js
@@ -4,59 +4,59 @@ document.addEventListener('DOMContentLoaded', function() {
   let dogList = document.getElementById('dog-breeds')
   let dogContainer = document.getElementById('dog-image-container')
 
-
-
-
-  fetch(imgUrl)
-  .then(res => res.json())
-  .then(dogs => {
-    let dogPics = Object.entries(dogs.message)
-
-    dogPics.forEach(dog => {
-      let img = document.createElement('img')
-      img.src = dog[1]
-
-      dogContainer.appendChild(img)
+  function displayDogs() {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(dogs => {
+      let dogPics = Object.entries(dogs.message)
+  
+      dogPics.forEach(dog => {
+        let img = document.createElement('img')
+        img.src = dog[1]
+  
+        dogContainer.appendChild(img)
+      })
     })
-  })
+  }
 
+function displayBreedList() {
   fetch(breedUrl)
   .then(res => res.json())
   .then(dogBreed => {
     let dogObj = dogBreed.message
-    for(const dog in dogObj){
+    for(const dogBreed in dogObj){
       let li = document.createElement('li')
 
-      li.innerText = dog
+      li.innerText = dogBreed
       dogList.appendChild(li)
 
-      li.addEventListener('click', (e) => {
+      li.addEventListener('click', () => {
         li.style.color = 'green'
       })
 
-      // console.log(dog[0])
+        function sortBreeds() {
+          let select = document.getElementById('breed-dropdown')
+          select.addEventListener('change', () => {
+            dogList.innerText = ''
+            let letter = select.value
 
-      let select = document.getElementById('breed-dropdown')
-
-      select.addEventListener('change', (e) => {
-        dogList.innerText = ''
-        let letter = select.value
-
-        for(const dog in dogObj) {
-          if(dog[0] === letter) {
-            let li = document.createElement('li')
-            li.innerText = dog
-            dogList.appendChild(li)
-          }
-        } 
-      })
-
-      //Grab select ID
-      //Target value / letter
-      //If selection letter === dog[0]
-      //clear UL innerHTML
-      //Re-add for breeds whos dog[0] === selection letter
-    }
+            for(const dogBreed in dogObj) {
+              if(dogBreed[0] === letter) {
+                let li = document.createElement('li')
+                li.innerText = dogBreed
+                dogList.appendChild(li)
+                li.addEventListener('click', () => {
+                  li.style.color = 'green'
+              })
+            }
+          } 
+        })
+      }
+    } sortBreeds()
   })
+}
+
+  displayBreedList()
+  displayDogs()
 })
 
